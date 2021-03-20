1eee3b20d0fa835bd92a6f3bf2aea08871065e80
WANG Haoxian <sharockys@gmail.com>
2021-03-20 18:09:02 +0100

    Getting start to config tweet.

---
+++ listentweet/data/get_tweet.py
@@ -0,0 +1,7 @@
+import tweepy
+import dotenv
+import os
+
+project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+dotenv_path = os.path.join(project_dir, ".env")
+dotenv.load_dotenv(dotenv_path)
