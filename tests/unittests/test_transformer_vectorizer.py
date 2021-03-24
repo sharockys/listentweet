8b2260597429a823a2d669561f1266235242b8e6
WANG Haoxian <sharockys@gmail.com>
2021-03-24 18:36:04 +0100

    fixed minor bug.

---
+++ tests/unittests/test_transformer_vectorizer.py
@@ -0,0 +1,42 @@
+import transformers
+
+
+def test_transformer_vectorizer():
+    from listentweet.pipelines.transformer_vectorizer import (
+        TransformersVectorizer,
+    )
+
+    # test loading
+    vectorizer = TransformersVectorizer("camembert-base")
+    assert isinstance(
+        vectorizer.tokenizer,
+        transformers.models.camembert.tokenization_camembert_fast.CamembertTokenizerFast,
+    )
+
+    assert isinstance(
+        vectorizer.model,
+        transformers.models.camembert.modeling_camembert.CamembertModel,
+    )
+
+    # test samples
+    sentences = ["Je ne veux pas travailler", "mais je n'ai pas d'argent"]
+
+    # test basic vectorization on one sentence
+    single_vector = vectorizer(sentences[0])
+    assert single_vector.shape == (1, 768)
+
+    # test basic vectorization on batch
+    batch_vector = vectorizer(sentences)
+    assert batch_vector.shape == (2, 768)
+
+    # test averaged vector on one sentence
+    mean_single_vector = vectorizer.vectorize_sentences_by_average_pool(
+        sentences[0]
+    )
+    assert mean_single_vector.shape == (1, 768)
+
+    # test averaged vector on batch
+    batch_mean_vector = vectorizer.vectorize_sentences_by_average_pool(
+        sentences
+    )
+    assert batch_mean_vector.shape == (2, 768)
